282957bef56fedc87cd2d1f7b79ee0f68b2d57fe
Liam P. White <byteslice@airmail.cc>
2019-08-28 13:34:36 -0400

    context: roles

---
+++ lib/philomena/roles/role.ex
@@ -0,0 +1,21 @@
+defmodule Philomena.Roles.Role do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "roles" do
+    field :name, :string
+
+    # fixme: rails polymorphic relation
+    field :resource_id, :integer
+    field :resource_type, :string
+
+    timestamps(inserted_at: :created_at)
+  end
+
+  @doc false
+  def changeset(role, attrs) do
+    role
+    |> cast(attrs, [])
+    |> validate_required([])
+  end
+end
