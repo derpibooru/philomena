da1237a32ac2c5a88464c9eeaee350d16f29524c
byte[] <byteslice@airmail.cc>
2019-11-11 12:25:51 -0500

    primitive avatar support

--- config/config.exs
+++ config/config.exs
@@ -12,7 +12,8 @@ config :philomena,
   elasticsearch_url: "http://localhost:9200",
   password_pepper: "dn2e0EpZrvBLoxUM3gfQveBhjf0bG/6/bYhrOyq3L3hV9hdo/bimJ+irbDWsuXLP",
   otp_secret_key: "Wn7O/8DD+qxL0X4X7bvT90wOkVGcA90bIHww4twR03Ci//zq7PnMw8ypqyyT/b/C",
-  image_url_root: "/img"
+  image_url_root: "/img",
+  avatar_url_root: "/avatars"
 
 config :philomena, :pow,
   user: Philomena.Users.User,
--- lib/philomena_web/views/user_attribution_view.ex
+++ lib/philomena_web/views/user_attribution_view.ex
@@ -12,6 +12,22 @@ defmodule PhilomenaWeb.UserAttributionView do
     |> Integer.to_string(16)
   end
 
+  def anonymous_avatar(_object, class \\ "avatar--100px") do
+    img_tag(Routes.static_path(PhilomenaWeb.Endpoint, "/images/no_avatar.svg"), class: class)
+  end
+
+  def user_avatar(object, class \\ "avatar--100px") do
+    if object.user.avatar do
+      img_tag(avatar_url_root() <> object.user.avatar, class: class)
+    else
+      anonymous_avatar(object, class)
+    end
+  end
+
+  defp avatar_url_root do
+    Application.get_env(:philomena, :avatar_url_root)
+  end
+
   defp anonymous_name_salt do
     Application.get_env(:philomena, :anonymous_name_salt)
     |> to_string()
