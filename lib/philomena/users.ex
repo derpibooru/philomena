31d637cca0f882533157400041c20d9ecb1d2b12
byte[] <byteslice@airmail.cc>
2019-11-14 20:15:07 -0500

    remove role from context

--- lib/philomena/users.ex
+++ lib/philomena/users.ex
@@ -8,6 +8,10 @@ defmodule Philomena.Users do
 
   alias Philomena.Users.User
 
+  use Pow.Ecto.Context,
+    repo: Repo,
+    user: User
+
   @doc """
   Returns the list of users.
 
@@ -100,101 +104,5 @@ defmodule Philomena.Users do
   """
   def change_user(%User{} = user) do
     User.changeset(user, %{})
-  end
-
-  alias Philomena.Users.Role
-
-  @doc """
-  Returns the list of users_roles.
-
-  ## Examples
-
-      iex> list_users_roles()
-      [%Role{}, ...]
-
-  """
-  def list_users_roles do
-    Repo.all(Role)
-  end
-
-  @doc """
-  Gets a single role.
-
-  Raises `Ecto.NoResultsError` if the Role does not exist.
-
-  ## Examples
-
-      iex> get_role!(123)
-      %Role{}
-
-      iex> get_role!(456)
-      ** (Ecto.NoResultsError)
-
-  """
-  def get_role!(id), do: Repo.get!(Role, id)
-
-  @doc """
-  Creates a role.
-
-  ## Examples
-
-      iex> create_role(%{field: value})
-      {:ok, %Role{}}
-
-      iex> create_role(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def create_role(attrs \\ %{}) do
-    %Role{}
-    |> Role.changeset(attrs)
-    |> Repo.insert()
-  end
-
-  @doc """
-  Updates a role.
-
-  ## Examples
-
-      iex> update_role(role, %{field: new_value})
-      {:ok, %Role{}}
-
-      iex> update_role(role, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def update_role(%Role{} = role, attrs) do
-    role
-    |> Role.changeset(attrs)
-    |> Repo.update()
-  end
-
-  @doc """
-  Deletes a Role.
-
-  ## Examples
-
-      iex> delete_role(role)
-      {:ok, %Role{}}
-
-      iex> delete_role(role)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_role(%Role{} = role) do
-    Repo.delete(role)
-  end
-
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking role changes.
-
-  ## Examples
-
-      iex> change_role(role)
-      %Ecto.Changeset{source: %Role{}}
-
-  """
-  def change_role(%Role{} = role) do
-    Role.changeset(role, %{})
   end
 end
