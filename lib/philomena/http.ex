5e5e3d3b45f1b6bd8b1a9db1247cbbc8c363c515
byte[] <byteslice@airmail.cc>
2019-11-28 19:56:36 -0500

    prepend host header

--- lib/philomena/http.ex
+++ lib/philomena/http.ex
@@ -2,17 +2,24 @@ defmodule Philomena.Http do
   @user_agent ["User-Agent": "Mozilla/5.0 (X11; Philomena; Linux x86_64; rv:70.0) Gecko/20100101 Firefox/70.0"]
 
   def get!(url, headers \\ [], options \\ []) do
-    headers = Keyword.merge(@user_agent, headers)
+    headers = Keyword.merge(@user_agent, headers) |> add_host(url)
     options = Keyword.merge(options, proxy: proxy_host())
 
     HTTPoison.get!(url, headers, options)
   end
 
   def head!(url, headers \\ [], options \\ []) do
-    headers = Keyword.merge(@user_agent, headers)
+    headers = Keyword.merge(@user_agent, headers) |> add_host(url)
     options = Keyword.merge(options, proxy: proxy_host())
 
     HTTPoison.head!(url, headers, options)
+  end
+
+  # Add host for caching proxies, since hackney doesn't do it for us
+  defp add_host(headers, url) do
+    %{host: host} = URI.parse(url)
+
+    Keyword.merge(["Host": host], headers)
   end
 
   defp proxy_host do
