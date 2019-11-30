84bba9062b2c99dbf8bdefe4ffcd7e93180c4f3d
byte[] <byteslice@airmail.cc>
2019-11-30 01:39:44 -0500

    redirect correctly

--- lib/philomena_web/controllers/channel_controller.ex
+++ lib/philomena_web/controllers/channel_controller.ex
@@ -25,6 +25,15 @@ defmodule PhilomenaWeb.ChannelController do
 
     if user, do: Channels.clear_notification(channel, user)
 
-    redirect(conn, external: channel.url)
+    redirect(conn, external: url(channel))
   end
+
+  defp url(%{type: "LivestreamChannel", short_name: short_name}),
+    do: "http://www.livestream.com/#{short_name}"
+  defp url(%{type: "PicartoChannel", short_name: short_name}),
+    do: "https://picarto.tv/#{short_name}"
+  defp url(%{type: "PiczelChannel", short_name: short_name}),
+    do: "https://piczel.tv/watch/#{short_name}"
+  defp url(%{type: "TwitchChannel", short_name: short_name}),
+    do: "https://www.twitch.tv/#{short_name}"
 end
