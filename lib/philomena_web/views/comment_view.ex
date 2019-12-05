2b0e65e85ed9664adf505213e6510a110104dc3c
Luna D <cod7777@yandex.ru>
2019-12-05 14:59:40 -0500

    truncate FP and use helpers

--- lib/philomena_web/views/comment_view.ex
+++ lib/philomena_web/views/comment_view.ex
@@ -1,3 +1,19 @@
 defmodule PhilomenaWeb.CommentView do
   use PhilomenaWeb, :view
+
+  def link_to_ip(ip) do
+    if ip do
+      ip
+    else
+      "N/A"
+    end
+  end
+
+  def link_to_fingerprint(fp) do
+    if fp do
+      fp |> String.slice(0..6)
+    else
+      "N/A"
+    end
+  end
 end
