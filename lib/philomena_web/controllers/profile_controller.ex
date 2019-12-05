3d94deb7e57a4157be89960ac5d81d25073c1da0
byte[] <byteslice@airmail.cc>
2019-12-04 23:53:56 -0500

    add more recents to profile page

--- lib/philomena_web/controllers/profile_controller.ex
+++ lib/philomena_web/controllers/profile_controller.ex
@@ -3,7 +3,12 @@ defmodule PhilomenaWeb.ProfileController do
 
   alias PhilomenaWeb.ImageLoader
   alias Philomena.Users.User
+  alias Philomena.Galleries.Gallery
+  alias Philomena.Posts.Post
+  alias Philomena.Comments.Comment
   alias Philomena.Interactions
+  alias Philomena.Repo
+  import Ecto.Query
 
   plug :load_and_authorize_resource, model: User, only: :show, id_field: "slug", preload: [awards: :badge, public_links: :tag]
 
@@ -25,6 +30,49 @@ defmodule PhilomenaWeb.ProfileController do
         pagination: %{page_number: 1, page_size: 6}
       )
 
+    recent_comments =
+      Comment.search_records(
+        %{
+          query: %{
+            bool: %{
+              must: [
+                %{term: %{user_id: user.id}},
+                %{term: %{anonymous: false}},
+                %{term: %{hidden_from_users: false}}
+              ]
+            }
+          }
+        },
+        %{page_size: 3},
+        Comment |> preload(user: [awards: :badge], image: :tags)
+      )
+      |> Enum.filter(&Canada.Can.can?(current_user, :show, &1.image))
+
+    recent_posts =
+      Post.search_records(
+        %{
+          query: %{
+            bool: %{
+              must: [
+                %{term: %{user_id: user.id}},
+                %{term: %{anonymous: false}},
+                %{term: %{hidden_from_users: false}}
+              ]
+            }
+          }
+        },
+        %{page_size: 6},
+        Post |> preload(user: [awards: :badge], topic: :forum)
+      )
+      |> Enum.filter(&Canada.Can.can?(current_user, :show, &1.topic))
+
+    recent_galleries =
+      Gallery
+      |> where(creator_id: ^user.id)
+      |> preload([:creator, thumbnail: :tags])
+      |> limit(5)
+      |> Repo.all()
+
     interactions =
       Interactions.user_interactions([recent_uploads, recent_faves], current_user)
 
@@ -35,6 +83,9 @@ defmodule PhilomenaWeb.ProfileController do
       interactions: interactions,
       recent_uploads: recent_uploads,
       recent_faves: recent_faves,
+      recent_comments: recent_comments,
+      recent_posts: recent_posts,
+      recent_galleries: recent_galleries,
       layout_class: "layout--wide"
     )
   end
