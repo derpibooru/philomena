76dce674ea6943a5694a11b740dc1d68a38e5ac7
Luna D <cod7777@yandex.ru>
2019-12-05 15:06:18 -0500

    ip/fp info on posts

--- lib/philomena_web/views/app_view.ex
+++ lib/philomena_web/views/app_view.ex
@@ -103,4 +103,20 @@ defmodule PhilomenaWeb.AppView do
     ])
     |> to_string()
   end
+
+  def link_to_ip(ip) do
+    if ip do
+      ip
+    else
+      "N/A"
+    end
+  end
+
+  def link_to_fingerprint(fp) do
+    if fp do
+      fp |> String.slice(0..6)
+    else
+      "N/A"
+    end
+  end
 end
