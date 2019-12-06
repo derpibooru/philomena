36e44146bcce3146ccb7535a63d55974df2262c3
Luna D <cod7777@yandex.ru>
2019-12-06 16:38:36 -0500

    update fa, autoprefixer, normalize

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -24,13 +24,32 @@ module.exports = {
     sass: {
       mode: 'native',
       options: {
-        includePaths: ['css', 'node_modules/font-awesome/scss']
+        includePaths: ['css', 'node_modules/@fortawesome/fontawesome-free/scss']
       }
     },
     copycat: {
-      fonts: ['node_modules/font-awesome/fonts'],
+      fonts: ['node_modules/@fortawesome/fontawesome-free/webfonts'],
       verbose: false,
       onlyChanged: true
+    },
+    postcss: {
+      processors: [
+        require('autoprefixer')({
+          overrideBrowserslist: [
+            'last 2 Android versions',
+            'last 2 Chrome versions',
+            'last 2 ChromeAndroid versions',
+            'last 2 Edge versions',
+            'last 1 Explorer version',
+            'last 1 ExplorerMobile versions',
+            'last 2 Firefox versions',
+            'last 2 FirefoxAndroid versions',
+            'last 2 iOS versions',
+            'last 2 Opera versions'
+          ],
+          add: true
+        })
+      ]
     }
   },
   conventions: {
