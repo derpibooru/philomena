1fbe3bd555765a57bd9ba18363d0980537645870
byte[] <byteslice@airmail.cc>
2019-12-14 22:25:11 -0500

    ensure flash is fetched

--- lib/philomena_web/plugs/not_authorized_plug.ex
+++ lib/philomena_web/plugs/not_authorized_plug.ex
@@ -7,6 +7,7 @@ defmodule PhilomenaWeb.NotAuthorizedPlug do
   def call(conn), do: call(conn, nil)
   def call(conn, _opts) do
     conn
+    |> Controller.fetch_flash()
     |> Controller.put_flash(:error, "You can't access that page.")
     |> Controller.redirect(external: conn.assigns.referrer)
     |> Conn.halt()
--- lib/philomena_web/plugs/not_found_plug.ex
+++ lib/philomena_web/plugs/not_found_plug.ex
@@ -7,6 +7,7 @@ defmodule PhilomenaWeb.NotFoundPlug do
   def call(conn), do: call(conn, nil)
   def call(conn, _opts) do
     conn
+    |> Controller.fetch_flash()
     |> Controller.put_flash(:error, "Couldn't find what you were looking for!")
     |> Controller.redirect(external: conn.assigns.referrer)
     |> Conn.halt()
