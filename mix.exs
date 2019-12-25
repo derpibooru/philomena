ef4a9154b6875aa9074c2f1dd7f463d6929d66c0
byte[] <byteslice@airmail.cc>
2019-12-25 08:38:05 -0500

    Revert "add debug pow"
    This reverts commit a34d12b0e2b1d81165d7b0be0ede22e2f07cd780.

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule Philomena.MixProject do
       {:plug_cowboy, "~> 2.0"},
       {:phoenix_slime, "~> 0.12.0"},
       {:ecto_network, "~> 1.1"},
-      {:pow, github: "liamwhite/pow", ref: "f654b36"},
+      {:pow, "~> 1.0.15"},
       {:bcrypt_elixir, "~> 2.0"},
       {:pot, "~> 0.10.1"},
       {:secure_compare, "~> 0.1.0"},
