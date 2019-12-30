0c7f13932bb451f9f1cc2d5b9c81230f62d77fbb
byte[] <byteslice@airmail.cc>
2019-12-30 18:32:24 -0500

    add deleted toggle to main search form

--- lib/philomena/schema/search.ex
+++ lib/philomena/schema/search.ex
@@ -1,10 +1,10 @@
 defmodule Philomena.Schema.Search do
   alias Philomena.Images.Query
-  import Search.String
+  alias Search.String
   import Ecto.Changeset
 
   def validate_search(changeset, field, user, watched \\ false) do
-    query = changeset |> get_field(field) |> normalize()
+    query = changeset |> get_field(field) |> String.normalize()
     output = Query.compile(user, query, watched)
 
     case output do
--- lib/philomena_web/views/search_view.ex
+++ lib/philomena_web/views/search_view.ex
@@ -2,4 +2,5 @@ defmodule PhilomenaWeb.SearchView do
   use PhilomenaWeb, :view
 
   def scope(conn), do: Philomena.ImageScope.scope(conn)
+  def hides_images?(conn), do: can?(conn, :hide, %Philomena.Images.Image{})
 end
