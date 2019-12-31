92387c96fcbd7c2630a39a04e366a7fcafa85df7
SomewhatDamaged <not.2.me.damaged@gmail.com>
2020-01-01 02:03:37 +1100

    Permission lockout
    Lockout of all data if destroyed.

--- lib/philomena_web/comment_json.ex
+++ lib/philomena_web/comment_json.ex
@@ -2,12 +2,14 @@ defmodule PhilomenaWeb.CommentJson do
   alias PhilomenaWeb.UserAttributionView
 
   def as_json(comment) do
-    %{
-      id: comment.id,
-      image_id: comment.image_id,
-      user_id: if(not comment.anonymous and not comment.image.hidden_from_users, do: comment.user_id),
-      author: if(not comment.image.hidden_from_users, do: if(comment.anonymous or is_nil(comment.user), do: UserAttributionView.anonymous_name(comment), else: comment.user.name)),
-      body: if(not comment.image.hidden_from_users and not comment.hidden_from_users, do: comment.body)
-    }
+    if not comment.destroyed_content do
+      %{
+        id: comment.id,
+        image_id: comment.image_id,
+        user_id: if(not comment.anonymous and not comment.image.hidden_from_users, do: comment.user_id),
+        author: if(not comment.image.hidden_from_users, do: if(comment.anonymous or is_nil(comment.user), do: UserAttributionView.anonymous_name(comment), else: comment.user.name)),
+        body: if(not comment.image.hidden_from_users and not comment.hidden_from_users, do: comment.body)
+      }
+    end
   end
 end
