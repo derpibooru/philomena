670d47bb34dc62baeaed6d21c0aa0e5396f33dfc
SomewhatDamaged <not.2.me.damaged@gmail.com>
2020-01-02 12:31:04 +1100

    Cleaned up
    Consolidated queries
    Fixed spacing
    Removed unneeded aliases

--- lib/philomena_web/controllers/api/json/topics_controller.ex
+++ lib/philomena_web/controllers/api/json/topics_controller.ex
@@ -1,17 +1,22 @@
-defmodule PhilomenaWeb.Api.Json.TopicsController do
+defmodule PhilomenaWeb.Api.Json.TopicController do
   use PhilomenaWeb, :controller
 
   alias PhilomenaWeb.TopicJson
   alias Philomena.Topics.Topic
-  alias Philomena.Forums.Forum
   alias Philomena.Repo
   import Ecto.Query
 
-  def show(conn, %{"forums_id" => forum_id, "id" => id}) do
+  def show(conn, %{"forum_id" => forum_id, "id" => id}) do
+
     topic = 
       Topic
-      |> where(forum_id: ^forum_id)
-      |> where(id: ^id)
+      |> where(slug: ^id)
+      |> where(hidden_from_users: false)
+      |> join(:inner, [t], _ in assoc(t, :forum))
+      |> where([_t, f], f.access_level == "normal")
+      |> where([_t, f], f.short_name == ^forum_id)
+      |> order_by(desc: :sticky, desc: :last_replied_to_at)
+      |> preload([:poll, :forum, :user, last_post: :user])
       |> preload([:user, :forum])
       |> Repo.one()
 
@@ -21,36 +26,23 @@ defmodule PhilomenaWeb.Api.Json.TopicsController do
         |> put_status(:not_found)
         |> text("")
 
-      topic.hidden_from_users or topic.forum.access_level != "normal" ->
-        conn
-        |> put_status(:forbidden)
-        |> text("")
-
       true ->
         json(conn, %{topic: TopicJson.as_json(topic)})
-
     end
   end
 
-  def index(conn, %{"forums_id" => id}) do
-    forum = 
-      Forum
-      |> where(id: ^id)
-      |> Repo.one()
-    cond do
-      forum.access_level != "normal" ->
-        conn
-        |> put_status(:forbidden)
-        |> text("")
-      true ->
-        topics =
-          Topic
-          |> where(forum_id: ^id)
-          |> where(hidden_from_users: false)
-          |> order_by(desc: :sticky, desc: :last_replied_to_at)
-          |> preload([:poll, :forum, :user, last_post: :user])
-          |> Repo.paginate(conn.assigns.scrivener)
-        json(conn, %{topic: Enum.map(topics, &TopicJson.as_json/1)})
-    end
+  def index(conn, %{"forum_id" => id}) do
+
+    topics =
+      Topic
+      |> where(hidden_from_users: false)
+      |> join(:inner, [t], _ in assoc(t, :forum))
+      |> where([_t, f], f.access_level == "normal")
+      |> where([_t, f], f.short_name == ^id)
+      |> order_by(desc: :sticky, desc: :last_replied_to_at)
+      |> preload([:poll, :forum, :user, last_post: :user])
+      |> Repo.paginate(conn.assigns.scrivener)
+
+    json(conn, %{topic: Enum.map(topics, &TopicJson.as_json/1)})
   end
 end
