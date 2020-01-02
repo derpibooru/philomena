023fe511fb2d23cc5dc6a19572a0ce6d5c7ce1c2
SomewhatDamaged <not.2.me.damaged@gmail.com>
2020-01-02 13:46:37 +1100

    Cleaned up
    Grouped clauses

--- lib/philomena_web/controllers/api/json/posts_controller.ex
+++ lib/philomena_web/controllers/api/json/posts_controller.ex
@@ -9,12 +9,12 @@ defmodule PhilomenaWeb.Api.Json.PostController do
   def show(conn, %{"forum_id" => forum_id, "topic_id" => topic_id, "id" => post_id}) do
     post = 
       Post
+      |> join(:inner, [p], _ in assoc(p, :topic))
+      |> join(:inner, [_p, t], _ in assoc(t, :forum))
       |> where(id: ^post_id)
       |> where(destroyed_content: false)
-      |> join(:inner, [p], _ in assoc(p, :topic))
       |> where([_p, t], t.hidden_from_users == false)
       |> where([_p, t], t.slug == ^topic_id)
-      |> join(:inner, [_p, t], _ in assoc(t, :forum))
       |> where([_p, _t, f], f.access_level == "normal")
       |> where([_p, _t, f], f.short_name == ^forum_id)      
       |> preload([:user, :topic])
@@ -32,35 +32,21 @@ defmodule PhilomenaWeb.Api.Json.PostController do
   end
 
   def index(conn, %{"forum_id" => forum_id, "topic_id" => topic_id} = params) do
-    page = paginate(params["page"])
+    page = conn.assigns.pagination.page_number
     posts = 
       Post
-      |> where(destroyed_content: false)
       |> join(:inner, [p], _ in assoc(p, :topic))
+      |> join(:inner, [_p, t], _ in assoc(t, :forum))
+      |> where(destroyed_content: false)
       |> where([_p, t], t.hidden_from_users == false)
       |> where([_p, t], t.slug == ^topic_id)
-      |> join(:inner, [_p, t], _ in assoc(t, :forum))
       |> where([_p, _t, f], f.access_level == "normal")
-      |> where([_p, _t, f], f.short_name == ^forum_id)      
+      |> where([_p, _t, f], f.short_name == ^forum_id)
       |> where([p], p.topic_position >= ^(25 * (page - 1)) and p.topic_position < ^(25 * page))
       |> order_by(asc: :topic_position)
       |> preload([:user, :topic])
       |> Repo.all()
 
     json(conn, %{posts: Enum.map(posts, &PostJson.as_json/1), page: page})
-  end
-
-  defp paginate(page) do
-    cond do
-      is_nil(page) ->
-        1
-
-      String.to_integer(page) > 0 ->
-        String.to_integer(page)
-
-      true ->
-        1
-
-    end
   end
 end
