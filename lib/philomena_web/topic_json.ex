1a145ddfd09c2b8317b5bb0f12e6b66a724aff88
SomewhatDamaged <not.2.me.damaged@gmail.com>
2020-01-02 13:55:45 +1100

    Cleaned up
    removed `id`
    `locked`

--- lib/philomena_web/topic_json.ex
+++ lib/philomena_web/topic_json.ex
@@ -1,9 +1,8 @@
 defmodule PhilomenaWeb.TopicJson do
   alias PhilomenaWeb.UserAttributionView
 
-  def as_json(%{hidden_from_users: true} = topic) do
+  def as_json(%{hidden_from_users: true} = _topic) do
     %{
-      id: topic.id,
       slug: nil,
       title: nil,
       post_count: nil,
@@ -17,15 +16,13 @@ defmodule PhilomenaWeb.TopicJson do
   end
   def as_json(topic) do
     %{
-      id: topic.id,
       slug: topic.slug,
       title: topic.title,
       post_count: topic.post_count,
       view_count: topic.view_count,
       sticky: topic.sticky,
       last_replied_to_at: topic.last_replied_to_at,
-      forum_id: topic.forum_id,
-      locked: if(is_nil(topic.locked_at), do: :false, else: :true),
+      locked: not is_nil(topic.locked_at),
       user_id: if(not topic.anonymous, do: topic.user.id),
       author: if(topic.anonymous or is_nil(topic.user), do: UserAttributionView.anonymous_name(topic), else: topic.user.name)
     }
