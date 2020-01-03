d89715907e8996a94fbac8c9ec94eebe6babb4de
SomewhatDamaged <not.2.me.damaged@gmail.com>
2020-01-03 23:16:56 +1100

    Fix for distance
    Fixes the `distance` to search not being passed through to `ImageReverse.images()`
    
    `set_scraper_cache` passes the `distance` through into `params`
    `create` normalizes and inserts the result into the params passed to `ImageReverse.images()`
    `normalize_dist` checks if there is a `distance` set, adds a leading 0 to it (to account for values like `.5`, which would crash `Float.parse()`), and turns it back into a well-formatted string for `ImageReverse.images()`

--- lib/philomena_web/controllers/api/json/search/reverse_controller.ex
+++ lib/philomena_web/controllers/api/json/search/reverse_controller.ex
@@ -10,6 +10,7 @@ defmodule PhilomenaWeb.Api.Json.Search.ReverseController do
   def create(conn, %{"image" => image_params}) do
     images =
       image_params
+      |> Map.put("distance", conn.params["distance"])
       |> ImageReverse.images()
       |> Enum.map(&ImageJson.as_json(conn, &1))
 
@@ -18,11 +19,21 @@ defmodule PhilomenaWeb.Api.Json.Search.ReverseController do
   end
 
   defp set_scraper_cache(conn, _opts) do
+
     params =
       conn.params
       |> Map.put("image", %{})
+      |> Map.put("distance", normalize_dist(conn.params))
       |> Map.put("scraper_cache", conn.params["url"])
 
     %{conn | params: params}
   end
+
+  defp normalize_dist(%{"distance" => distance}) do
+    "0" <> distance
+    |> Float.parse()
+    |> elem(0)
+    |> Float.to_string()
+  end
+  defp normalize_dist(_dist), do: "0.25"
 end
