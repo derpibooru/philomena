d7c206263b7c582da406041070ba31869c5b6c9c
SomewhatDamaged <not.2.me.damaged@gmail.com>
2020-01-04 22:51:48 +1100

    Added routes
    `/api/v1/images/:image_id/comments`
    `/api/v1/images/:image_id/comments/:comment_id`

--- lib/philomena_web/controllers/api/json/comment_controller.ex
+++ lib/philomena_web/controllers/api/json/comment_controller.ex
@@ -6,11 +6,14 @@ defmodule PhilomenaWeb.Api.Json.CommentController do
   alias Philomena.Repo
   import Ecto.Query
 
-  def show(conn, %{"id" => id}) do
+  def show(conn, %{"image_id" => image_id, "id" => id}) do
     comment = 
       Comment
-      |> where(id: ^id)
       |> preload([:image, :user])
+      |> join(:inner, [p], _ in assoc(p, :image))
+      |> where(id: ^id)
+      |> where(destroyed_content: false)
+      |> where([_p, t], t.hidden_from_users == false and t.id == ^image_id)
       |> Repo.one()
 
     cond do
@@ -19,14 +22,48 @@ defmodule PhilomenaWeb.Api.Json.CommentController do
         |> put_status(:not_found)
         |> text("")
 
-      comment.image.hidden_from_users ->
+      true ->
+        json(conn, %{comment: CommentJson.as_json(comment)})
+    end
+  end
+  def show(conn, %{"id" => id}) do
+    comment = 
+      Comment
+      |> where(id: ^id)
+      |> where(destroyed_content: false)
+      |> preload([:image, :user])
+      |> Repo.one()
+
+    cond do
+      is_nil(comment) ->
         conn
-        |> put_status(:forbidden)
+        |> put_status(:not_found)
         |> text("")
 
       true ->
         json(conn, %{comment: CommentJson.as_json(comment)})
+    end
+  end
 
+  def index(conn, %{"image_id" => image_id}) do
+    page = conn.assigns.pagination.page_number
+    comments = 
+      Comment
+      |> preload([:image, :user])
+      |> join(:inner, [p], _ in assoc(p, :image))
+      |> where(destroyed_content: false)
+      |> where([_p, t], t.hidden_from_users == false and t.id == ^image_id)
+      |> Repo.all()
+      |> IO.inspect
+
+    case comments do
+      [] ->
+        conn
+        |> put_status(:not_found)
+        |> text("")
+
+      _ ->
+        json(conn, %{comments: Enum.map(comments, &CommentJson.as_json/1), page: page, total: hd(comments).image.comments_count})
     end
   end
 end
