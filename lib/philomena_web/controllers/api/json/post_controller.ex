7961b64afc7ed455c29e8c0dda67fd68c82ccb9c
SomewhatDamaged <not.2.me.damaged@gmail.com>
2020-01-07 18:10:01 +1100

    Adding route controller
    Adding route for
    `/api/v1/json/posts/:id`

---
+++ lib/philomena_web/controllers/api/json/post_controller.ex
@@ -0,0 +1,31 @@
+defmodule PhilomenaWeb.Api.Json.PostController do
+  use PhilomenaWeb, :controller
+
+  alias PhilomenaWeb.PostJson
+  alias Philomena.Posts.Post
+  alias Philomena.Repo
+  import Ecto.Query
+
+  def show(conn, %{"id" => post_id}) do
+    post = 
+      Post
+      |> join(:inner, [p], _ in assoc(p, :topic))
+      |> join(:inner, [_p, t], _ in assoc(t, :forum))
+      |> where(id: ^post_id)
+      |> where(destroyed_content: false)
+      |> where([_p, t], t.hidden_from_users == false)
+      |> where([_p, _t, f], f.access_level == "normal")      
+      |> preload([:user, :topic])
+      |> Repo.one()
+
+    cond do
+      is_nil(post) ->
+        conn
+        |> put_status(:not_found)
+        |> text("")
+
+      true ->
+        json(conn, %{post: PostJson.as_json(post)})
+    end
+  end
+end
