d35b7a2b0ebef0a6d07d556da8da2336a890f1bf
Nebbie Zebbie <nebbiezebbie@gmail.com>
2020-01-31 20:10:15 +0000

    Allow repairing deleted images

--- lib/philomena_web/controllers/image/repair_controller.ex
+++ lib/philomena_web/controllers/image/repair_controller.ex
@@ -6,7 +6,6 @@ defmodule PhilomenaWeb.Image.RepairController do
 
   plug PhilomenaWeb.CanaryMapPlug, create: :hide
   plug :load_and_authorize_resource, model: Image, id_name: "image_id", persisted: true
-  plug :verify_not_deleted
 
   def create(conn, _params) do
     spawn(fn ->
@@ -16,17 +15,5 @@ defmodule PhilomenaWeb.Image.RepairController do
     conn
     |> put_flash(:info, "Repair job started.")
     |> redirect(to: Routes.image_path(conn, :show, conn.assigns.image))
-  end
-
-  defp verify_not_deleted(conn, _opts) do
-    case conn.assigns.image.hidden_from_users do
-      true ->
-        conn
-        |> put_flash(:error, "Cannot repair a hidden image.")
-        |> redirect(to: Routes.image_path(conn, :show, conn.assigns.image))
-
-      _false ->
-        conn
-    end
   end
 end
