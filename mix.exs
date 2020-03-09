77cde55ff6b7eec709b29f9f183c148b4e71b4b2
byte[] <byteslice@airmail.cc>
2020-03-09 18:21:18 -0400

    elixir upgrade, npm upgrades, hex upgrades

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule Philomena.MixProject do
       {:phoenix, "~> 1.4.9"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
-      {:ecto_sql, "~> 3.3"},
+      {:ecto_sql, "~> 3.3.0"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
