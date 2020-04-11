b0eb5d7792366d13f193cd89922270fff56626e4
Nick Kanel <kanel.nick@gmail.com>
2020-04-11 14:17:42 -0400

    formatting

--- lib/philomena_web/plugs/captcha_plug.ex
+++ lib/philomena_web/plugs/captcha_plug.ex
@@ -1,7 +1,7 @@
 defmodule PhilomenaWeb.CaptchaPlug do
   alias Philomena.Captcha
-  alias Phoenix.Controller
-  alias Plug.Conn
+  import Plug.Conn
+  import Phoenix.Controller
 
   def init([]), do: false
 
@@ -19,31 +19,29 @@ defmodule PhilomenaWeb.CaptchaPlug do
 
       false ->
         conn
-        |> Controller.put_flash(
+        |> put_flash(
           :error,
           "There was an error verifying you're not a robot. Please try again."
         )
-        |> do_failure_response()
-        |> Conn.halt()
+        |> do_failure_response(ajax?(conn))
+        |> halt()
     end
   end
 
   defp maybe_check_captcha(conn, _user), do: conn
 
-  defp do_failure_response(conn) do
-    case ajax?(conn) do
-      true ->
-        conn
-        |> Conn.put_status(:multiple_choices)
-        |> Controller.text("")
-      false ->
-        conn
-        |> Controller.redirect(external: conn.assigns.referrer)
-    end
+  defp do_failure_response(conn, true) do
+    conn
+    |> put_status(:multiple_choices)
+    |> text("")
+  end
+
+  defp do_failure_response(conn, _false) do
+    redirect(conn, external: conn.assigns.referrer)
   end
 
   def ajax?(conn) do
-    case Conn.get_req_header(conn, "x-requested-with") do
+    case get_req_header(conn, "x-requested-with") do
       [value] -> String.downcase(value) == "xmlhttprequest"
       _ -> false
     end
