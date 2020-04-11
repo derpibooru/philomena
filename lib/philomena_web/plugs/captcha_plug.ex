3f0a8f8dd7c641f15a24ef28e044bdf0bc8de9d0
Nick Kanel <kanel.nick@gmail.com>
2020-04-11 13:39:31 -0400

    handle form posts and xhr posts differently on captcha failure

--- lib/philomena_web/plugs/captcha_plug.ex
+++ lib/philomena_web/plugs/captcha_plug.ex
@@ -23,10 +23,29 @@ defmodule PhilomenaWeb.CaptchaPlug do
           :error,
           "There was an error verifying you're not a robot. Please try again."
         )
-        |> Controller.redirect(external: conn.assigns.referrer)
+        |> do_failure_response()
         |> Conn.halt()
     end
   end
 
   defp maybe_check_captcha(conn, _user), do: conn
+
+  defp do_failure_response(conn) do
+    case ajax?(conn) do
+      true ->
+        conn
+        |> Conn.put_status(:multiple_choices)
+        |> Controller.text("")
+      false ->
+        conn
+        |> Controller.redirect(external: conn.assigns.referrer)
+    end
+  end
+
+  def ajax?(conn) do
+    case Conn.get_req_header(conn, "x-requested-with") do
+      [value] -> String.downcase(value) == "xmlhttprequest"
+      _ -> false
+    end
+  end
 end
