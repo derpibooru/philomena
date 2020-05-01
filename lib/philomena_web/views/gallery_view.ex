8f590920827b75f4f60a87236df20d9a7da23f91
byte[] <byteslice@airmail.cc>
2020-05-01 15:51:28 -0400

    fixes #77: restore cross-page gallery organization

--- lib/philomena_web/views/gallery_view.ex
+++ lib/philomena_web/views/gallery_view.ex
@@ -5,6 +5,19 @@ defmodule PhilomenaWeb.GalleryView do
 
   def scope(conn), do: ImageScope.scope(conn)
 
+  def sortable_classes(%{assigns: %{gallery_prev: prev, gallery_next: next}}) do
+    []
+    |> sortable_prev(prev)
+    |> sortable_next(next)
+    |> Enum.join(" ")
+  end
+
+  def sortable_prev(list, nil), do: list
+  def sortable_prev(list, _), do: ["js-sortable-has-prev" | list]
+
+  def sortable_next(list, nil), do: list
+  def sortable_next(list, _), do: ["js-sortable-has-next" | list]
+
   def show_subscription_link?(%{id: id}, %{id: id}), do: false
   def show_subscription_link?(_user1, _user2), do: true
 end
