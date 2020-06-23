97deb39062c0bb9aa984b4e475e3d73571b4056e
Joey <joey@crazyponies.net>
2020-06-23 21:52:50 +0000

    Functionality

--- lib/philomena_web/controllers/registration/deactivate_controller.ex
+++ lib/philomena_web/controllers/registration/deactivate_controller.ex
@@ -2,6 +2,7 @@ defmodule PhilomenaWeb.Registration.DeactivateController do
   use PhilomenaWeb, :controller
 
   alias Philomena.Users
+  alias Philomena.UserWipe
 
   plug PhilomenaWeb.FilterBannedUsersPlug
 
@@ -11,8 +12,16 @@ defmodule PhilomenaWeb.Registration.DeactivateController do
   end
 
   def create(conn, _params) do
+    user = conn.assigns.current_user
+
+    {:ok, user} = Users.deactivate_user(user, user)
+
+    spawn(fn ->
+      UserWipe.perform(user)
+    end)
+
     conn
-    |> put_flash(:info, "Nothing happened")
+    |> put_flash(:info, "Your account has been deactivated, and your PII will be removed from the database shortly.")
     |> redirect(to: "/")
   end
 
