b58bb87d7157566e5abca56c0627e26bf9d5b16b
byte[] <byteslice@airmail.cc>
2020-07-13 13:09:30 -0400

    fix staff tag showing for staff hiding a role

--- lib/philomena_web/views/source_change_view.ex
+++ lib/philomena_web/views/source_change_view.ex
@@ -4,7 +4,7 @@ defmodule PhilomenaWeb.SourceChangeView do
   def staff?(source_change),
     do:
       not is_nil(source_change.user) and not Philomena.Attribution.anonymous?(source_change) and
-        source_change.user.role != "user"
+        source_change.user.role != "user" and not source_change.user.hide_default_role
 
   def user_column_class(source_change) do
     case staff?(source_change) do
