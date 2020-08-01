0a09432744afacc426caf54b986d2082dd3dcd5f
alamais-df <27428175+alamais-df@users.noreply.github.com>
2020-08-01 08:22:35 -0400

    Modify notable check to use postgres function, allowing both query names and inserted names to pass through the same processing functions (punctuation and space stripping, downcasing).

--- lib/philomena/notable.ex
+++ lib/philomena/notable.ex
@@ -1,6 +1,7 @@
 defmodule Philomena.Notable do
 
   import Ecto.Query, warn: false
+  alias Ecto.Adapters.SQL
 
   alias Philomena.Repo
   alias Philomena.Notable.Name
@@ -17,17 +18,22 @@ defmodule Philomena.Notable do
   """
   def notable_name?(name) do
     if have_table() do
-      name = name |> String.downcase
-		  Repo.exists?(from n in Name,
-        where: fragment("notable_name_query(?)", n.name))
+      case nnquery(name) do
+        {:ok, res} ->
+          res.num_rows > 0
+        {:error, err} ->
+          false
+      end
     else
       false
     end
   end
 
-  defp have_table() do
-    alias Ecto.Adapters.SQL
+  defp nnquery(name) do
+    SQL.query(Repo, "SELECT * FROM notable_name_query($1)", [name])
+  end
 
+  defp have_table() do
     SQL.table_exists?(Repo, "notable_names")
   end
 end
