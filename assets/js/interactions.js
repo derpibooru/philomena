902241063d3aea6d7fd11735ecdb89d92fc82d3b
basisbit <hp.stefan@gmail.com>
2020-08-05 00:29:51 +0200

    fix link to sign in page
    commit cherry-picked from https://github.com/philomena-dev/philomena/commit/a81464f189fe3c196e74997d432fa0eeb1a38d0f
    
    Co-Authored-By: liamwhite <liamwhite@users.noreply.github.com>

--- assets/js/interactions.js
+++ assets/js/interactions.js
@@ -181,7 +181,7 @@ function bindInteractions() {
 
 function loggedOutInteractions() {
   [].forEach.call(document.querySelectorAll('.interaction--fave,.interaction--upvote,.interaction--downvote'),
-    a => a.setAttribute('href', '/session/new'));
+    a => a.setAttribute('href', '/sessions/new'));
 }
 
 function setupInteractions() {
