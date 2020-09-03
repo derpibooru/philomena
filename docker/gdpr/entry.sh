65c04e170225708bdb3d904043623564666f69cf
Chase22 <chase@mailbox.org>
2020-09-03 21:12:50 +0200

    Pass missing host and db variables into the gdpr-contaienr and map variables on startup

--- docker/gdpr/entry.sh
+++ docker/gdpr/entry.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+#set environment
+export PGHOST=$POSTGRES_HOST
+export PGPORT=$POSTGRES_PORT
+export PGDATABASE=$POSTGRES_DB
+export PGUSER=$POSTGRES_USER
+export PGPASSWORD=$POSTGRES_PASSWORD
+
+#print environment
+echo "Started with psql config"
+echo "PGHOST=$PGHOST"
+echo "PGPORT=$PGPORT"
+echo "PGDATABASE=$PGDATABASE"
+echo "PGUSER=$PGUSER"
+
 # start cron
 echo "Starting cron"
 /usr/sbin/crond -f -l 8
