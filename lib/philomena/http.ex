cc571a38c2d71a0ce90f659ad40af3c9df379bdd
Luna D <cod7777@yandex.ru>
2020-10-21 20:10:11 +0200

    bump app upload limit to 125MB

--- lib/philomena/http.ex
+++ lib/philomena/http.ex
@@ -12,7 +12,7 @@ defmodule Philomena.Http do
   end
 
   defp adapter_opts(opts) do
-    opts = Keyword.merge(opts, max_body: 100_000_000)
+    opts = Keyword.merge(opts, max_body: 125_000_000)
 
     case Application.get_env(:philomena, :proxy_host) do
       nil ->
