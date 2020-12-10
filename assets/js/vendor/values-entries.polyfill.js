214173095cba4f774a7784fd269fa8829e23b99c
byte[] <byteslice@airmail.cc>
2020-12-10 01:30:41 -0500

    since you asked so nicely

---
+++ assets/js/vendor/values-entries.polyfill.js
@@ -0,0 +1,13 @@
+// object-values | MIT | github.com/tc39/proposal-object-values-entries
+
+if (!Object.values) {
+  Object.values = function values(O) {
+    return reduce(keys(O), (v, k) => concat(v, typeof k === 'string' && isEnumerable(O, k) ? [O[k]] : []), []);
+  };
+}
+
+if (!Object.entries) {
+  Object.entries = function entries(O) {
+    return reduce(keys(O), (e, k) => concat(e, typeof k === 'string' && isEnumerable(O, k) ? [[k, O[k]]] : []), []);
+  };
+}
