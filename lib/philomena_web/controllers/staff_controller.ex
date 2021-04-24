533f2b27912887e813c6d3b60551047b40eff998
Luna D <cod7777@yandex.ru>
2021-04-24 13:17:27 +0200

    unavailable staff category

--- lib/philomena_web/controllers/staff_controller.ex
+++ lib/philomena_web/controllers/staff_controller.ex
@@ -13,24 +13,25 @@ defmodule PhilomenaWeb.StaffController do
       |> Repo.all()
 
     categories = [
-      Administrators: Enum.filter(users, &(&1.role == "admin")),
+      Administrators: Enum.filter(users, &(&1.role == "admin" and &1.hide_default_role == false)),
       "Technical Team":
         Enum.filter(
           users,
-          &(&1.role != "admin" and &1.secondary_role in ["Site Developer", "Devops"])
+          &(&1.role != "admin" and &1.secondary_role in ["Site Developer", "Devops"] and &1.hide_default_role == false)
         ),
       "Public Relations":
-        Enum.filter(users, &(&1.role != "admin" and &1.secondary_role == "Public Relations")),
+        Enum.filter(users, &(&1.role != "admin" and &1.secondary_role == "Public Relations" and &1.hide_default_role == false)),
       Moderators:
         Enum.filter(
           users,
-          &(&1.role == "moderator" and &1.secondary_role in [nil, ""])
+          &(&1.role == "moderator" and &1.secondary_role in [nil, ""] and &1.hide_default_role == false)
         ),
       Assistants:
         Enum.filter(
           users,
-          &(&1.role == "assistant" and &1.secondary_role in [nil, ""])
-        )
+          &(&1.role == "assistant" and &1.secondary_role in [nil, ""] and &1.hide_default_role == false)
+        ),
+      "Unavailable Staff": Enum.filter(users, &(&1.hide_default_role == true))
     ]
 
     render(conn, "index.html", title: "Site Staff", categories: categories)
