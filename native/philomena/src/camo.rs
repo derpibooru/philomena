35a227e9382856220fb08a82e863da7e67fe5463
Luna D <cod7777@yandex.ru>
2021-09-16 21:04:15 +0200

    use gocamo

--- native/philomena/src/camo.rs
+++ native/philomena/src/camo.rs
@@ -13,16 +13,14 @@ fn untrusted_host(url: Url, camo_host: String, camo_key: String) -> Option<Strin
     let camo_url = format!("https://{}", camo_host);
     let key = hmac::Key::new(hmac::HMAC_SHA1_FOR_LEGACY_USE_ONLY, camo_key.as_ref());
     let tag = hmac::sign(&key, url.to_string().as_bytes());
-    let encoded = base16::encode_lower(tag.as_ref());
+    let encoded = base64::encode_config(tag.as_ref(), base64::URL_SAFE_NO_PAD);
+    let encoded_url = base64::encode_config(url.as_ref(), base64::URL_SAFE_NO_PAD);
+    let path = format!("{}/{}", encoded, encoded_url);
 
     let mut camo_uri = Url::parse(&camo_url).ok()?;
-    camo_uri.set_path(&encoded);
+    camo_uri.set_path(&path);
     camo_uri.set_port(Some(443)).ok()?;
     camo_uri.set_scheme("https").ok()?;
-    camo_uri
-        .query_pairs_mut()
-        .clear()
-        .append_pair("url", &url.to_string());
 
     Some(camo_uri.to_string())
 }
