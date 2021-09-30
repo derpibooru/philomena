27af96fe3a78ce19d3cde02dbbc156cb308fb47d
SeinopSys <seinopsys@protonmail.com>
2021-09-30 23:56:38 +0200

    unrequire editor fields that weren't previously required

--- lib/philomena_web/views/markdown_view.ex
+++ lib/philomena_web/views/markdown_view.ex
@@ -4,4 +4,7 @@ defmodule PhilomenaWeb.MarkdownView do
   def anonymous_by_default?(conn) do
     conn.assigns.current_user.anonymous_by_default
   end
+
+  def required?(%{assigns: %{required: false}}), do: nil
+  def required?(_), do: true
 end
