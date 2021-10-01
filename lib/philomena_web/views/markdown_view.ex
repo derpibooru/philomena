a1e848514e425cb2b7fece2a7cc6dba1ca3d6ecc
SeinopSys <seinopsys@protonmail.com>
2021-10-01 02:31:25 +0200

    fix issues with the image upload description markdown input

--- lib/philomena_web/views/markdown_view.ex
+++ lib/philomena_web/views/markdown_view.ex
@@ -5,6 +5,9 @@ defmodule PhilomenaWeb.MarkdownView do
     conn.assigns.current_user.anonymous_by_default
   end
 
-  def required?(%{assigns: %{required: false}}), do: nil
+  def required?(required) when required == false, do: nil
   def required?(_), do: true
+
+  def add_classes(base_classes, new_classes) when is_binary(new_classes), do: "#{base_classes} #{new_classes}"
+  def add_classes(base_classes, _), do: base_classes
 end
