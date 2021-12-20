e9247b6f987e55c10f4015d512fa3620e33a9eb4
Luna D <cod7777@yandex.ru>
2021-12-20 21:45:23 +0100

    bump to pg14 for real now

--- priv/repo/structure.sql
+++ priv/repo/structure.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 13.4
--- Dumped by pg_dump version 13.4
+-- Dumped from database version 14.1
+-- Dumped by pg_dump version 14.1
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
