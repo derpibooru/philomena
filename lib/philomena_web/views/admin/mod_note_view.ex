a82664720fce65ee7b7223b3ed5847ba03d13919
byte[] <byteslice@airmail.cc>
2023-04-24 14:38:05 -0400

    Make mod notes show the tag name instead of the requesting user

--- lib/philomena_web/views/admin/mod_note_view.ex
+++ lib/philomena_web/views/admin/mod_note_view.ex
@@ -5,8 +5,8 @@ defmodule PhilomenaWeb.Admin.ModNoteView do
   alias Philomena.Reports.Report
   alias Philomena.DnpEntries.DnpEntry
 
-  def link_to_noted_thing(conn, %DnpEntry{requesting_user: user} = dnp_entry),
-    do: link("#{user.name}'s DNP entry", to: Routes.dnp_entry_path(conn, :show, dnp_entry))
+  def link_to_noted_thing(conn, %DnpEntry{tag: tag} = dnp_entry),
+    do: link("DNP entry for #{tag.name}", to: Routes.dnp_entry_path(conn, :show, dnp_entry))
 
   def link_to_noted_thing(conn, %Report{user: nil} = report),
     do: link("Report #{report.id}", to: Routes.admin_report_path(conn, :show, report))
