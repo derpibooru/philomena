3ffebd15332c7c20a6b02674f5cf6cfbdbe0da4d
Liam <byteslice@airmail.cc>
2024-04-08 12:46:54 -0400

    Fix dynamic typing mistake

--- lib/philomena_web/views/search_view.ex
+++ lib/philomena_web/views/search_view.ex
@@ -3,4 +3,10 @@ defmodule PhilomenaWeb.SearchView do
 
   def scope(conn), do: PhilomenaWeb.ImageScope.scope(conn)
   def hides_images?(conn), do: can?(conn, :hide, %Philomena.Images.Image{})
+
+  def override_display([{_tag, _description, dnp_entries}]) do
+    Enum.any?(dnp_entries)
+  end
+
+  def override_display(_), do: false
 end
