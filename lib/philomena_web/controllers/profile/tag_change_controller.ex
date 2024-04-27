eb79ee45d2d6c76992d6c190ed4ce0b2e38a39e1
liamwhite <liamwhite@users.noreply.github.com>
2024-04-27 14:00:54 -0400

    Tag change search (#234)
    * profile/tag_change: add search box to show only a single tag
    
    * Minor fixup
    
    ---------
    
    Co-authored-by: prg <prg@lacunae.de>

--- lib/philomena_web/controllers/profile/tag_change_controller.ex
+++ lib/philomena_web/controllers/profile/tag_change_controller.ex
@@ -3,6 +3,7 @@ defmodule PhilomenaWeb.Profile.TagChangeController do
 
   alias Philomena.Users.User
   alias Philomena.Images.Image
+  alias Philomena.Tags.Tag
   alias Philomena.TagChanges.TagChange
   alias Philomena.Repo
   import Ecto.Query
@@ -16,19 +17,27 @@ defmodule PhilomenaWeb.Profile.TagChangeController do
     tag_changes =
       TagChange
       |> join(:inner, [tc], i in Image, on: tc.image_id == i.id)
+      |> only_tag_join(params)
       |> where(
         [tc, i],
         tc.user_id == ^user.id and not (i.user_id == ^user.id and i.anonymous == true)
       )
       |> added_filter(params)
+      |> only_tag_filter(params)
       |> preload([:tag, :user, image: [:user, :sources, tags: :aliases]])
       |> order_by(desc: :id)
       |> Repo.paginate(conn.assigns.scrivener)
 
+    # params.permit(:added, :only_tag) ...
+    pagination_params =
+      [added: conn.params["added"], only_tag: conn.params["only_tag"]]
+      |> Keyword.filter(fn {k, _v} -> Map.has_key?(conn.params, "#{k}") end)
+
     render(conn, "index.html",
       title: "Tag Changes for User `#{user.name}'",
       user: user,
-      tag_changes: tag_changes
+      tag_changes: tag_changes,
+      pagination_params: pagination_params
     )
   end
 
@@ -39,5 +48,17 @@ defmodule PhilomenaWeb.Profile.TagChangeController do
     do: where(query, added: false)
 
   defp added_filter(query, _params),
+    do: query
+
+  defp only_tag_join(query, %{"only_tag" => only_tag}) when only_tag != "",
+    do: join(query, :inner, [tc], t in Tag, on: tc.tag_id == t.id)
+
+  defp only_tag_join(query, _params),
+    do: query
+
+  defp only_tag_filter(query, %{"only_tag" => only_tag}) when only_tag != "",
+    do: where(query, [_, _, t], t.name == ^only_tag)
+
+  defp only_tag_filter(query, _params),
     do: query
 end
