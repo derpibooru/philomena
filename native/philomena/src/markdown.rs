9c797e8d643c645b975ccd76a1bb0c7b96faf4fe
Liam <byteslice@airmail.cc>
2024-11-08 23:55:50 -0500

    Rust code refactor

--- native/philomena/src/markdown.rs
+++ native/philomena/src/markdown.rs
@@ -1,11 +1,9 @@
-use crate::camo;
-use comrak::ComrakOptions;
-use rustler::{MapIterator, Term};
+use crate::{camo, domains};
+use comrak::Options;
 use std::collections::HashMap;
-use std::env;
 
-fn common_options() -> ComrakOptions {
-    let mut options = ComrakOptions::default();
+fn common_options() -> Options {
+    let mut options = Options::default();
     options.extension.autolink = true;
     options.extension.table = true;
     options.extension.description_lists = true;
@@ -16,47 +14,24 @@ fn common_options() -> ComrakOptions {
     options.render.hardbreaks = true;
     options.render.github_pre_lang = true;
 
-    options.extension.camoifier = Some(|s| camo::image_url(s).unwrap_or_else(|| String::from("")));
-
-    if let Ok(domains) = env::var("SITE_DOMAINS") {
-        options.extension.philomena_domains = Some(
-            domains
-                .split(',')
-                .map(|s| s.to_string())
-                .collect::<Vec<String>>(),
-        );
-    }
+    options.extension.camoifier = Some(|s| camo::image_url_careful(&s));
+    options.extension.philomena_domains = domains::get();
 
     options
 }
 
-fn map_to_hashmap(map: Term) -> Option<HashMap<String, String>> {
-    Some(
-        MapIterator::new(map)?
-            .map(|(key, value)| {
-                let key: String = key.decode().unwrap_or_else(|_| String::from(""));
-                let value: String = value.decode().unwrap_or_else(|_| String::from(""));
-
-                (key, value)
-            })
-            .collect(),
-    )
-}
-
-pub fn to_html(input: String, reps: Term) -> String {
+pub fn to_html(input: &str, reps: HashMap<String, String>) -> String {
     let mut options = common_options();
     options.render.escape = true;
+    options.extension.philomena_replacements = Some(reps);
 
-    options.extension.philomena_replacements = map_to_hashmap(reps);
-
-    comrak::markdown_to_html(&input, &options)
+    comrak::markdown_to_html(input, &options)
 }
 
-pub fn to_html_unsafe(input: String, reps: Term) -> String {
+pub fn to_html_unsafe(input: &str, reps: HashMap<String, String>) -> String {
     let mut options = common_options();
     options.render.unsafe_ = true;
+    options.extension.philomena_replacements = Some(reps);
 
-    options.extension.philomena_replacements = map_to_hashmap(reps);
-
-    comrak::markdown_to_html(&input, &options)
+    comrak::markdown_to_html(input, &options)
 }
