4943abe39bb5040335fd0c12cd775891c90a5ff4
Liam <byteslice@airmail.cc>
2024-12-10 15:58:08 -0500

    Date header added in newer swoosh

--- lib/philomena/mailer.ex
+++ lib/philomena/mailer.ex
@@ -13,7 +13,6 @@ defmodule Philomena.Mailer do
     mail
     |> Email.from({"noreply", mailer_address()})
     |> Email.header("Message-ID", rfc2822_message_id())
-    |> Email.header("Date", rfc2822_date())
   end
 
   defp rfc2822_message_id do
@@ -23,10 +22,6 @@ defmodule Philomena.Mailer do
       |> String.downcase()
 
     "<#{id}.#{mailer_address()}>"
-  end
-
-  defp rfc2822_date do
-    Calendar.strftime(DateTime.utc_now(), "%a, %-d %b %Y %H:%M:%S %z")
   end
 
   defp mailer_address do
