1853c8984f85b03b1a9faad027c726f050acc83b
Liam <byteslice@airmail.cc>
2024-12-11 14:26:48 -0500

    Update swoosh to include fix for mua message-id

--- lib/philomena/mailer.ex
+++ lib/philomena/mailer.ex
@@ -10,18 +10,7 @@ defmodule Philomena.Mailer do
 
   @spec format_message(Email.t()) :: Email.t()
   def format_message(mail) do
-    mail
-    |> Email.from({"noreply", mailer_address()})
-    |> Email.header("Message-ID", rfc2822_message_id())
-  end
-
-  defp rfc2822_message_id do
-    id =
-      :crypto.strong_rand_bytes(16)
-      |> Base.encode16()
-      |> String.downcase()
-
-    "<#{id}.#{mailer_address()}>"
+    Email.from(mail, {"noreply", mailer_address()})
   end
 
   defp mailer_address do
