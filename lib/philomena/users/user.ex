5c347eccb68342e5418c7676a2bd886c3fa8e4c2
Liam <byteslice@airmail.cc>
2024-12-17 15:17:12 -0500

    rename cyan to teal

--- lib/philomena/users/user.ex
+++ lib/philomena/users/user.ex
@@ -605,7 +605,7 @@ defmodule Philomena.Users.User do
     do: user.otp_backup_codes |> Enum.reject(&Password.verify_pass(token, &1))
 
   def theme_colors do
-    ~W(red orange yellow blue green purple cyan pink gray)
+    ~W(red orange yellow blue green purple teal pink gray)
   end
 
   def theme_names do
