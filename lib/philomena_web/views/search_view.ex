801f67e69a11f2b9e6f229f70a8f431d7f80ba8d
mdashlw <mdashlw@gmail.com>
2025-03-03 04:10:58 +0000

    Update override display logic for tags

--- lib/philomena_web/views/search_view.ex
+++ lib/philomena_web/views/search_view.ex
@@ -4,9 +4,10 @@ defmodule PhilomenaWeb.SearchView do
   def scope(conn), do: PhilomenaWeb.ImageScope.scope(conn)
   def hides_images?(conn), do: can?(conn, :hide, %Philomena.Images.Image{})
 
-  def override_display([{_tag, _description, dnp_entries}]) do
-    Enum.any?(dnp_entries)
+  def override_display(conn, [{tag, _description, dnp_entries}]) do
+    tag.images_count > 0 or Enum.any?(dnp_entries) or
+      (present?(tag.mod_notes) and can?(conn, :edit, tag))
   end
 
-  def override_display(_), do: false
+  def override_display(_conn, _tags), do: false
 end
