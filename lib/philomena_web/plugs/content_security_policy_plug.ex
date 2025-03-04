3296bc0a918e90287c426e94a96bd7f552b719e1
MareStare <mare.stare.official@gmail.com>
2025-03-04 03:25:09 +0000

    Wrap default_connect_src as well

--- lib/philomena_web/plugs/content_security_policy_plug.ex
+++ lib/philomena_web/plugs/content_security_policy_plug.ex
@@ -75,15 +75,10 @@ defmodule PhilomenaWeb.ContentSecurityPolicyPlug do
     defp default_script_src(_host), do: "'self'"
   end
 
-  defp default_connect_src(host) do
-    # Same workaround as in `default_script_src/1`
-    is_vite_hmr = vite_hmr?(do: true, else: false)
-
-    if is_vite_hmr do
-      "'self' #{host}:5173 ws://#{host}:5173"
-    else
-      "'self'"
-    end
+  vite_hmr? do
+    defp default_connect_src(host), do: "'self' #{host}:5173 ws://#{host}:5173"
+  else
+    defp default_connect_src(_host), do: "'self'"
   end
 
   defp default_style_src, do: vite_hmr?(do: "'self' 'unsafe-inline'", else: "'self'")
