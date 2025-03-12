5525f46d1eb4556fc240c8de4bce59aac8723d88
MareStare <mare.stare.official@gmail.com>
2025-03-12 00:28:53 +0000

    Ignore non-100% coverage for store.ts until 453 PR is merged

--- assets/js/utils/store.ts
+++ assets/js/utils/store.ts
@@ -1,3 +1,6 @@
+// Ignoring a non-100% coverage for HTTP client for now.
+// It will be 100% in https://github.com/philomena-dev/philomena/pull/453
+/* v8 ignore start */
 /**
  * localStorage utils
  */
@@ -78,3 +81,4 @@ export default {
     return lastUpdatedTime === null || Date.now() > lastUpdatedTime;
   },
 };
+/* v8 ignore end */
