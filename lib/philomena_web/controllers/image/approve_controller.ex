a3f56c85654ea6fc5abb512ec69f1104907a72bc
mdashlw <mdashlw@gmail.com>
2025-04-16 10:01:11 +0000

    Image approval checks

--- lib/philomena_web/controllers/image/approve_controller.ex
+++ lib/philomena_web/controllers/image/approve_controller.ex
@@ -6,6 +6,7 @@ defmodule PhilomenaWeb.Image.ApproveController do
 
   plug PhilomenaWeb.CanaryMapPlug, create: :approve
   plug :load_and_authorize_resource, model: Image, id_name: "image_id", persisted: true
+  plug :verify_image_state
 
   def create(conn, _params) do
     image = conn.assigns.image
@@ -16,6 +17,27 @@ defmodule PhilomenaWeb.Image.ApproveController do
     |> put_flash(:info, "Image has been approved.")
     |> moderation_log(details: &log_details/2, data: image)
     |> redirect(to: ~p"/admin/approvals")
+  end
+
+  defp verify_image_state(conn, _opts) do
+    image = conn.assigns.image
+
+    cond do
+      image.approved ->
+        conn
+        |> put_flash(:error, "Someone else already approved this image.")
+        |> redirect(to: ~p"/admin/approvals")
+        |> halt()
+
+      image.hidden_from_users ->
+        conn
+        |> put_flash(:error, "Cannot approve a deleted image.")
+        |> redirect(to: ~p"/admin/approvals")
+        |> halt()
+
+      true ->
+        conn
+    end
   end
 
   defp log_details(_action, image) do
