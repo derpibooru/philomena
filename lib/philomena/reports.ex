6236508a1a929dae12a8bd59a27474a4b1bf4802
mdashlw <mdashlw@gmail.com>
2025-04-27 16:28:09 +0800

    Fix autoclosing reports overriding admin_id

--- lib/philomena/reports.ex
+++ lib/philomena/reports.ex
@@ -78,7 +78,7 @@ defmodule Philomena.Reports do
       {:error, %Ecto.Changeset{}}
 
   """
-  def create_report({reportable_type, reportable_id} = _type_and_id, attribution, attrs \\ %{}) do
+  def create_report({reportable_type, reportable_id}, attribution, attrs \\ %{}) do
     %Report{reportable_type: reportable_type, reportable_id: reportable_id}
     |> Report.creation_changeset(attrs, attribution)
     |> Repo.insert()
@@ -86,13 +86,13 @@ defmodule Philomena.Reports do
   end
 
   @doc """
-  Returns an `m:Ecto.Query` which updates all reports for the given `reportable_type`
+  Returns an `m:Ecto.Query` which updates all open reports for the given `reportable_type`
   and `reportable_id` to close them.
 
   Because this is only a query due to the limitations of `m:Ecto.Multi`, this must be
   coupled with an associated call to `reindex_reports/1` to operate correctly, e.g.:
 
-      report_query = Reports.close_system_report_query({"Image", image.id}, user)
+      report_query = Reports.close_report_query({"Image", image.id}, user)
 
       Multi.new()
       |> Multi.update_all(:reports, report_query, [])
@@ -109,13 +109,15 @@ defmodule Philomena.Reports do
 
   ## Examples
 
-      iex> close_system_report_query("Image", 1, %User{})
+      iex> close_report_query({"Image", 1}, %User{})
       #Ecto.Query<...>
 
   """
-  def close_report_query({reportable_type, reportable_id} = _type_and_id, closing_user) do
+  def close_report_query({reportable_type, reportable_id}, closing_user) do
     from r in Report,
-      where: r.reportable_type == ^reportable_type and r.reportable_id == ^reportable_id,
+      where:
+        r.reportable_type == ^reportable_type and r.reportable_id == ^reportable_id and
+          r.open == true,
       select: r.id,
       update: [set: [open: false, state: "closed", admin_id: ^closing_user.id]]
   end
@@ -130,7 +132,7 @@ defmodule Philomena.Reports do
       {:ok, %Report{}}
 
   """
-  def create_system_report({reportable_type, reportable_id} = _type_and_id, category, reason) do
+  def create_system_report({reportable_type, reportable_id}, category, reason) do
     attrs = %{
       reason: reason,
       category: category,
