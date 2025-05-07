47b7b2d9276336c96fdb993545ebd6573b75da46
Luna D. <luna@nighty.cloud>
2025-05-07 18:07:46 +0200

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Philomena.MixProject do
   def project do
     [
       app: :philomena,
-      version: "1.1.0",
+      version: "1.1.1",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
