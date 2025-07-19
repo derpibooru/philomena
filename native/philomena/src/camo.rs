f1ec652a3a0b565b8280f2afda343abf4e54f54b
mdashlw <mdashlw@gmail.com>
2025-07-19 18:09:37 +0800

    Inline format args to fix CI

--- native/philomena/src/camo.rs
+++ native/philomena/src/camo.rs
@@ -12,12 +12,12 @@ fn trusted_host(mut url: Url) -> Option<String> {
 }
 
 fn untrusted_host(url: Url, camo_host: &str, camo_key: &str) -> Option<String> {
-    let camo_url = format!("https://{}", camo_host);
+    let camo_url = format!("https://{camo_host}");
     let key = hmac::Key::new(hmac::HMAC_SHA1_FOR_LEGACY_USE_ONLY, camo_key.as_ref());
     let tag = hmac::sign(&key, url.to_string().as_bytes());
     let encoded = URL_SAFE_NO_PAD.encode(tag.as_ref());
     let encoded_url = URL_SAFE_NO_PAD.encode(url.as_ref());
-    let path = format!("{}/{}", encoded, encoded_url);
+    let path = format!("{encoded}/{encoded_url}");
 
     let mut camo_uri = Url::parse(&camo_url).ok()?;
     camo_uri.set_path(&path);
