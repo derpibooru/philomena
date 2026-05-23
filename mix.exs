93015e836e04022317e3cc33f4f7d675bac129a4
Liam <byteslice@airmail.cc>
2026-05-23 18:59:22 -0400

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Philomena.MixProject do
   def project do
     [
       app: :philomena,
-      version: "1.2.4",
+      version: "1.2.5",
       elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
