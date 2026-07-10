7f4ec11baf16924db56d2fd21701feea37e005e0
Luna Fox <luna@nighty.cloud>
2026-07-10 16:30:31 +0200

    fix incorrect moderator permission checks

--- lib/philomena_web/controllers/admin/user/wipe_controller.ex
+++ lib/philomena_web/controllers/admin/user/wipe_controller.ex
@@ -22,7 +22,7 @@ defmodule PhilomenaWeb.Admin.User.WipeController do
   end
 
   defp verify_authorized(conn, _opts) do
-    if Canada.Can.can?(conn.assigns.current_user, :index, User) do
+    if Canada.Can.can?(conn.assigns.current_user, :edit, %User{}) do
       conn
     else
       PhilomenaWeb.NotAuthorizedPlug.call(conn)
